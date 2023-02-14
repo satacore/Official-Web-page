da8b6d5fec5fd4332656c5e57be0478ca8bb05f1
Matias Roccazzella <matias.a.re@hotmail.com>
2023-02-14 18:27:23 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola como esta
+          Hola como esta Varsaliko
         </p>
         <a
           className="App-link"

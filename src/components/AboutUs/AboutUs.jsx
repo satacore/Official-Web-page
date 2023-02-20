1868cf63264a8a1821fac8cd6c31b5f5ee75d438
Matias Roccazzella <matias.a.re@hotmail.com>
2023-02-20 17:52:23 -0300

    commit 20/02/22 .4

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -8,7 +8,9 @@ const AboutUs = () => {
             <p className='p1'>About Us</p>
             <p className='p2'>..</p>
         </div>
-        
+        <div className='descriptionContainer'>
+            <p>Our software development company is a dynamic and innovative group of young and talented individuals who bring a fresh perspective to the industry. We are dedicated to combining creativity and the latest technology to deliver cutting-edge software solutions. Despite our youth, our team is highly professional and has a deep understanding of the software development process with > 2 years of experience, ensuring that all projects are delivered on time and to the highest standard.</p>
+        </div>
     </div>
   )
 }

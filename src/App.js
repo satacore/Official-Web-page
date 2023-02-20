f582c0e4b44006380a1e7a5ba8470a5e1f045f38
Santiago Valero <santiagovalero99@gmail.com>
2023-02-20 16:41:14 -0300

    20/02/23 .3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx'
+import Footer from './components/Footer/Footer.jsx'
 import SPA from './components/SPA/SPA.jsx';
 
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<SPA/>} />
       </Routes>
+    <Footer/>
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './footer.css'
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <header>
+       <nav>
+          <a href='/#'>© 2022 Satacore | All rights reserved | Privacy | Terms of Use | Desinged By: Satacore</a>
+       </nav>
+    </header>
+
+
+  )
+}
+export default Footer

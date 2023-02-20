a77a5ab46e0bc4fc9cb806c0d5c85f7021bad293
Santiago Valero <santiagovalero99@gmail.com>
2023-02-20 16:46:10 -0300

    20/02/23 .4

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <header>
        <nav>
-          <a href='/#'>© 2022 Satacore | All rights reserved | Privacy | Terms of Use | Desinged By: Satacore</a>
+          <a href='/#'>© 2023 Satacore | All rights reserved | Privacy | Terms of Use | Desinged By: Satacore</a>
        </nav>
     </header>
 

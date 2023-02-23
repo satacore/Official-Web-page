75ce747d84118c5f877073d119b7d07ee839ccdc
Santiago Valero <santiagovalero99@gmail.com>
2023-02-23 16:39:00 -0300

    23/02/23 .2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='homeContainer'>
         <div className='sloganContainer'>
-            <h1 className='slogan'>CREÁ, INNOVÁ, SOÑÁ</h1>
+            <h1 className='slogan'>SOÑÁ, PROYECTÁ, INNOVÁ</h1>
             <h1 className='slogaSubtitle'><span>Satacore</span> lo hace posible.</h1>
         </div>
     </div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,10 +9,10 @@ import Logo from '../../assets/Navbarlogo.png'
       <header>
         <img src={Logo} alt="" />
         <nav>
-            <p><Link to="aboutus" spy={true} smooth={true} offstet={50} duration={500} >ABOUT US</Link></p>
-            <p><Link to="services" spy={true} smooth={true} offstet={50} duration={500} >SERVICES</Link></p>
-            <p><Link to="clients" spy={true} smooth={true} offstet={50} duration={500} >CLIENTS</Link></p>
-            <p><Link to="contact" spy={true} smooth={true} offstet={50} duration={500} >CONTACT</Link></p>
+            <p><Link to="aboutus" spy={true} smooth={true} offstet={50} duration={500} ><button>ABOUT US</button></Link></p>
+            <p><Link to="services" spy={true} smooth={true} offstet={50} duration={500} ><button>SERVICES</button></Link></p>
+            <p><Link to="clients" spy={true} smooth={true} offstet={50} duration={500} ><button>CLIENTS</button></Link></p>
+            <p><Link to="contact" spy={true} smooth={true} offstet={50} duration={500} ><button>CONTACT</button></Link></p>
         </nav>
       </header>
 
